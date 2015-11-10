c6ee5eadddf6fa29a7079011c0cc6bf6f728582c
Andres <andrescampoe@gmail.com>
2015-11-10 14:49:52 -0500

    index

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include 'controlador/ControladorAdministrador.php';
-
-$objeto = new ControladorAdministrador();
-$objeto->insertar($_POST["id"], $_POST["fullName"], $_POST["email"], $_POST["password"]);
-
-?>
